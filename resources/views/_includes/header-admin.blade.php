027f7812aad56929c7b35185170e73348883d031
IbukunOluwa Shotubo <mcblessing1@gmail.com>
2019-02-19 14:34:16 +0100

    Changed page titles in header and header-admin blade to just OAcademy
    So that we simply see OAgric
    
    Signed-off-by: IbukunOluwa Shotubo <mcblessing1@gmail.com>

--- resources/views/_includes/header-admin.blade.php
+++ resources/views/_includes/header-admin.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>OAcademy | Front-End Web Developer</title>
+	<title>OAcademy</title>
 	<link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
 	<link rel="stylesheet" href="{{url('assets/fonts/font-awesome.min.css')}}">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
