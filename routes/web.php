5dc7742ab14836876dd981f4c0f7cda8cd8a0abd
chiemeke ifeanyi <ifeanyi@toolzacademy.com>
2019-05-15 15:30:08 +0100

    fixed routing issue

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'Activities@appointments');
+Route::get('/', 'Activities@appointments')->middleware('auth');
 
 
 Route::middleware('auth')->prefix('client')->group(function () {
